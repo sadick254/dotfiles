d77d82eb317f6a2b14d020660d301a10c4413c54
sadick254 <sadickjunior@gmail.com>
2023-02-25 07:49:17 +0300

    Include lualine

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,56 +1,56 @@
-return require('packer').startup(function (use)
-	use 'wbthomason/packer.nvim'
-  	use 'numToStr/Comment.nvim'
-	use 'sainnhe/edge'
-	use {
-		'nvim-treesitter/nvim-treesitter',
-		run = function()
-			pcall(require('nvim-treesitter.install').update { with_sync = true })
-		end,
-	}
+return require('packer').startup(function(use)
+	    use 'wbthomason/packer.nvim'
+	    use 'numToStr/Comment.nvim'
+	    use 'sainnhe/edge'
+	    use {
+	        'nvim-treesitter/nvim-treesitter',
+	        run = function()
+		        pcall(require('nvim-treesitter.install').update { with_sync = true })
+	        end,
+	    }
 
-	use { -- Additional text objects via treesitter
-		'nvim-treesitter/nvim-treesitter-textobjects',
-		after = 'nvim-treesitter',
-	}
-	use {
-		'nvim-tree/nvim-tree.lua',
-		requires = {
-			'nvim-tree/nvim-web-devicons', -- optional, for file icons
-		},
-		tag = 'nightly' -- optional, updated every week. (see issue #1193)
-	}
+	    use { -- Additional text objects via treesitter
+	        'nvim-treesitter/nvim-treesitter-textobjects',
+	        after = 'nvim-treesitter',
+	    }
+	    use {
+	        'nvim-tree/nvim-tree.lua',
+	        requires = {
+	            'nvim-tree/nvim-web-devicons', -- optional, for file icons
+	        },
+	        tag = 'nightly' -- optional, updated every week. (see issue #1193)
+	    }
 
-	-- Fuzzy Finder (files, lsp, etc)
-	use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
-	-- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
-	use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
+	    -- Fuzzy Finder (files, lsp, etc)
+	    use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
+	    -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
+	    use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
 
-	use { -- LSP Configuration & Plugins
-		'neovim/nvim-lspconfig',
-		requires = {
-			-- Automatically install LSPs to stdpath for neovim
-			'williamboman/mason.nvim',
-			'williamboman/mason-lspconfig.nvim',
+	    use { -- LSP Configuration & Plugins
+	        'neovim/nvim-lspconfig',
+	        requires = {
+	            -- Automatically install LSPs to stdpath for neovim
+	            'williamboman/mason.nvim',
+	            'williamboman/mason-lspconfig.nvim',
 
-			-- Useful status updates for LSP
-			'j-hui/fidget.nvim',
+	            -- Useful status updates for LSP
+	            'j-hui/fidget.nvim',
 
-			-- Additional lua configuration, makes nvim stuff amazing
-			'folke/neodev.nvim',
-		},
-	}
-	use { -- Autocompletion
-		'hrsh7th/nvim-cmp',
-		requires = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
-	}
+	            -- Additional lua configuration, makes nvim stuff amazing
+	            'folke/neodev.nvim',
+	        },
+	    }
+	    use { -- Autocompletion
+	        'hrsh7th/nvim-cmp',
+	        requires = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
+	    }
 
-		-- Git related plugins
-	use 'tpope/vim-fugitive'
-	use 'tpope/vim-rhubarb'
-	use 'lewis6991/gitsigns.nvim'
-	use 'lukas-reineke/indent-blankline.nvim'
-	use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
-	use 'sainnhe/sonokai'
-
-end)
+	    -- Git related plugins
+	    use 'tpope/vim-fugitive'
+	    use 'tpope/vim-rhubarb'
+	    use 'lewis6991/gitsigns.nvim'
+	    use 'lukas-reineke/indent-blankline.nvim'
+	    use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
+	    use 'sainnhe/sonokai'
+	    use 'nvim-lualine/lualine.nvim'
+    end)
